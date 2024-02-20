ad8372c9b0d00b161c79ebd56ac8eb6bc3f7d4b0
Ali Abdullah Diri <help.applusinc@gmail.com>
2024-02-21 02:59:04 +0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -2,22 +2,16 @@
 const mysql = require('mysql2');
 
 
-  const connection = mysql.createConnection({
+  const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'apay',
     password: '',
     charset: 'utf8',
-    connectTimeout : 5000,
-    connectionLimit :1
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+
   });
 
-  connection.connect((error) => {
-    if (error) {
-      console.error('Error connecting to MySQL database:', error);
-    } else {
-      console.log("Mysql Database'e bağlanıldı."
-       );
-    }
-  }); 
 module.exports = { connection }; 
