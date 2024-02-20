67609a9996e7523c4b4dac2353d3f53002915d6e
Ali Abdullah Diri <help.applusinc@gmail.com>
2024-02-20 16:15:11 +0300

    Delete node_modules directory

--- node_modules/@nowpaymentsio/nowpayments-api-js/src/actions/get-currencies/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Error } from '../../types';
-export interface GetCurrenciesReturn {
-    currencies: [string];
-}
-declare const getCurrencies: ({ apiKey }: {
-    apiKey: string;
-}) => Promise<GetCurrenciesReturn | Error>;
-export default getCurrencies;
--- node_modules/@nowpaymentsio/nowpayments-api-js/src/actions/status/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Error } from '../../types';
-export interface StatusReturn {
-    message: string;
-}
-declare const status: () => Promise<StatusReturn | Error>;
-export default status;
